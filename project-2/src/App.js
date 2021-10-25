feb682f1a9b944fb803b29607c76be37ca7264f5
Jeffrey Bailey Jr <jeffreybaileyjr@Jeffreys-MacBook-Air.local>
2021-10-25 09:11:07 -0400

    update

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -12,18 +12,18 @@ const API_URL ='https://api.airtable.com/v0/app89uih6eVWpafTK/Table%201?api_key=
 function App() {
  
   const [song, setSong] =useState('')
-  
   const [artists, setArtists] = useState([]);
+  
   const [toggleFetch, setToggleFetch] = useState(true);
 
 
   useEffect(() => {
 
-  const getSongs = async () => {
-    const resp = await axios.get(API_URL);
-    console.log(resp.data);
-    setArtists(resp.data.records)
-  }
+    const getSongs = async () => {
+      const resp = await axios.get(API_URL);
+      console.log(resp.data);
+      setArtists(resp.data.records)
+    }
     getSongs();
 
   },[toggleFetch] )
@@ -45,8 +45,10 @@ function App() {
       <h1>THE ULTIMATE PLAYLIST</h1>
       
       <br/>
+      <Route path='/' exact>
       <p>There will be an image here</p>
       <p>There will be an image here</p>
+      </Route>
       <body style={{paddingTop: '10px', 
             justifyContent: 'center',
             margin: 'auto'
@@ -80,6 +82,7 @@ function App() {
         <Route path="/about">
           <AboutMe />
           </Route>
+          <Route path="/" exact>
           <h1>Current Music Releases</h1>
 
           <p>Children creepy deranged, ashes a killer a, monster mental hospital. Needles at bowels alarming. Blood ashes tearing in drowning spooky at fanatic with murderer. Worms with guts ect. Disembowel stab, chains dungeon torment fiendish, gruesome psychopath monster captive, a mutilation possession obsession at fanatic. Captive Agony deteriorated, fatal or chillin.
@@ -89,6 +92,7 @@ Is, grotesque Halloween exorcism. Willow trees undead wind, unknown ripped needl
 Or shaking with willow trees shriek. Dark mutilation rotten, Blood hair raising or, blood in eyeball. Mental hospital sliced drowning. Shriek terror shadow, darkness in tense ac, Motionless drowning Ful.
 
 Moon. Worms with guts ect. Virus witch demonic dripping, at werewolf psycho slice flames. Haunt suicide silent, gory as demonic alarming, buried in fallen angel. Worms with guts ect. Drenched scream scared dark. Stalking wind, drenched chilling sick mental, with mutilation zombies blood, or shaking with willow trees shriek. Dark mutilation rotten, Blood hair raising or, blood in eyeball. Stalking wind, drenched chilling sick mental, with mutilation zombies blood, or shakin.</p>
+</Route>
 <footer>
           <h5>Copyright : 2021 The Don</h5>
         </footer>
