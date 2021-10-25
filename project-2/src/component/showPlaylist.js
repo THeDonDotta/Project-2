f9f3b57015964891595e26403c5ec07075e126d3
Jeffrey Bailey Jr <jeffreybaileyjr@Jeffreys-MacBook-Air.local>
2021-10-25 11:47:30 -0400

    update

--- project-2/src/component/showPlaylist.js
+++ project-2/src/component/showPlaylist.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Route, Link } from 'react-router-dom'
 import Card from '@mui/material/Card'
+import Rating from '@mui/material/Rating'
 
 const API_URL ='https://api.airtable.com/v0/app89uih6eVWpafTK/Table%201?api_key=keyZHnK73JWzkEwP6'
 
@@ -19,6 +20,7 @@ return(
         
         <p>Song: {artist.fields.song}</p>
         </Card>
+        <Rating name="size-medium" defaultValue={4} />
         <br/>
     </div>
 )
