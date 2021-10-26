54f66d2062a1cbaeef2a0318984b8129444d6983
Jeffrey Bailey Jr <jeffreybaileyjr@Jeffreys-MacBook-Air.local>
2021-10-26 14:54:55 -0400

    update

--- src/component/showPlaylist.js
+++ src/component/showPlaylist.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Route, Link } from 'react-router-dom'
+// import axios from 'axios';
+import { useState } from 'react';
+// import { Route, Link } from 'react-router-dom'
 import Card from '@mui/material/Card'
 import Rating from '@mui/material/Rating'
 
-const API_URL ='https://api.airtable.com/v0/app89uih6eVWpafTK/Table%201?api_key=keyZHnK73JWzkEwP6'
+// const API_URL ='https://api.airtable.com/v0/app89uih6eVWpafTK/Table%201?api_key=keyZHnK73JWzkEwP6'
 
 const ShowPlaylist = ({artist, song})  => {
 
-    const [artists, setArtists] = useState([]);
-    const [toggleFetch, setToggleFetch] = useState(true);
+    // const [artists, setArtists] = useState([]);
+    // const [toggleFetch, setToggleFetch] = useState(true);
 
 
 return(
