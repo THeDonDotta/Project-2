894f795407f2ff719a40cf6c5218c3345bd05648
Jeffrey Bailey Jr <jeffreybaileyjr@Jeffreys-MacBook-Air.local>
2021-10-26 07:37:32 -0400

    update

--- project-2/src/component/AboutMe.js
+++ project-2/src/component/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe =() => {
 
 return (
     <div>
-        <a href="https://imgur.com/2qVSzql"><img src="https://i.imgur.com/2qVSzql.png" title="source: imgur.com" className="logo"/></a>
+        <img src="https://i.imgur.com/2qVSzql.png" title="source: imgur.com" className="logo"/>
 
         <p style={{color: 'white'}}>This app is built for the music lover. If you love exploring new music this is the site for you. Find new songs you've never heard of before and expand your taste.</p>
     </div>
